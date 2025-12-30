19bd5ea981037af4d7161c582c512704c13a4951
kuwamin <kuwamin1119@gmail.com>
2025-12-30 12:31:28 +0900

    編集：軽微な修正

--- Yearly.py
+++ Yearly.py
@@ -3,12 +3,16 @@ import Game
 import ResetResult
 import SummaryResult
 
+# 変数
+gameNumber = 143    #試合数
+
+
 # 事前処理
 ResetResult.test()
 
 
 # 年単位（143試合）実行
-for i in range(143):
+for i in range(gameNumber):
     Game.test()
 
 # 事後処理
